4560e8148df300234b46b56bf4daeb3ec999b044
Sam Burkhard <samryanburk@icloud.com>
2021-10-21 21:16:49 -0400

    Initial Commit

---
+++ BeanRatio/BeanRatioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeanRatioApp.swift
+//  BeanRatio
+//
+//  Created by Sam Burkhard on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BeanRatioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeanRatio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeanRatio
+//
+//  Created by Sam Burkhard on 10/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
